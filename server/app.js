e907a6f924db244ceb0f9963581f0bd00007d890
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2023-02-22 11:11:08 +0200

    CODE: Added database for books

---
+++ server/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require("mongoose")
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/books');
+
+mongoose.set('strictQuery', true)
+var app = express();
+
+const mongoDB = "mongodb://localhost:27017/testdb"
+mongoose.connect(mongoDB)
+mongoose.Promise = Promise
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "MongoDB connection error"))
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+module.exports = app;
---
+++ server/routes/books.js
@@ -0,0 +1,21 @@
+var express = require('express');
+const bodyParser = require("body-parser")
+var router = express.Router();
+const Book = require("../models/Books")
+
+/* GET users listing. */
+router.post('/books', function(req, res, next) {
+
+    Book.create({
+        name: req.body.name,
+        author: req.body.author,
+        pages: req.body.pages
+    },(err,ok)=>{
+        if(err) throw(err);
+        return res.send("ok")
+    })
+
+    
+});
+
+module.exports = router;
